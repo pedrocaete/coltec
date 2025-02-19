904a6dcff54272cd5fcd8e00d3ac60bfe5cb8429
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2025-02-19 16:40:14 -0300

    from the class

---
+++ 2ano/Aula 1/aula1/Program.cs
@@ -0,0 +1,16 @@
+﻿using CAS;
+using System.Numerics;
+
+Expressao a = 10;
+Expressao b = "b";
+
+Expressao soma = new Soma(a, b);
+
+Console.WriteLine(soma);
+
+Expressao x = "x";
+Expressao y = x*x*x+10;
+Console.WriteLine(y);
+
+Expressao derivada = y.Derivar((Simbolo)x);
+Console.WriteLine(derivada);
