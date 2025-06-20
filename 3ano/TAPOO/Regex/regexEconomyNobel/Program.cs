6c61818f746212760496758e8091ec2647e769d7
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2025-06-20 13:17:01 -0300

    .

---
+++ 3ano/TAPOO/Regex/regexEconomyNobel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+string json = File.ReadAllText("prize.json");
+
+string pattern = @"""category"":""economics"".*?""laureates"":\s*\[(.*?)\]";
+MatchCollection matches = Regex.Matches(json, pattern, RegexOptions.Singleline);
+
+List<string> names = new List<string>();
+
+foreach (Match match in matches)
+{
+    string contentLaureates = match.Groups[1].Value;
+
+    string namePattern = @"""firstname"":""([^""]+)""";
+    MatchCollection namesMatches = Regex.Matches(contentLaureates, namePattern);
+
+    foreach (Match nameMatch in namesMatches)
+    {
+        string name = nameMatch.Groups[1].Value;
+        names.Add(name);
+    }
+}
+
+Console.WriteLine("Ganhadores do Nobel de Economia:");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
---
+++ 3ano/TAPOO/Regex/regexPassword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static string? Password;
+    public static void Main(string[] args)
+    {
+        string regexPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+=_\-{}\[\]:;""'?<>,.]).{7,16}$";
+        while (true)
+        {
+            Console.WriteLine("Digite a senha a ser verificada:");
+            Password = Console.ReadLine();
+            if (Regex.IsMatch(Password, regexPattern))
+            {
+                Console.WriteLine("Senha forte!");
+                return;
+            }
+            Console.WriteLine("Senha fraca, tente novamente");
+        }
+    }
+}
